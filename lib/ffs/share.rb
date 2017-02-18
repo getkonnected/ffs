084a03d04c522e984bcfc3cc23d349f21e273e8c
Hunter Braun <hunter.braun@gmail.com>
2017-02-17 18:13:54 -0600

    Debug

--- lib/ffs/share.rb
+++ lib/ffs/share.rb
@@ -110,7 +110,7 @@ module FFS
       uri = Addressable::URI.parse("https://api-ssl.bitly.com/v3/shorten?access_token=#{FFS.configuration.bitly_api_key}&longUrl=#{long_link}")
       res = JSON.parse(Net::HTTP.get(uri))
 
-      res['data']['url']
+      res['data']
     end
   end
 end
