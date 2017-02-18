52a450fee7121905490c3d7c528eb0a6bb34efa2
Hunter Braun <hunter.braun@gmail.com>
2017-02-17 18:01:27 -0600

    Fix typo

--- lib/ffs/share.rb
+++ lib/ffs/share.rb
@@ -107,7 +107,7 @@ module FFS
 
     def shorten_with_bitly(res)
       long_link = JSON.parse(res)['previewLink'].gsub(/&d=1/, '')
-      uri = Adressable::URI.parse("https://api-ssl.bitly.com/v3/shorten?access_token=#{FFS.configuration.bitly_api_key}&longUrl=#{long_link}")
+      uri = Addressable::URI.parse("https://api-ssl.bitly.com/v3/shorten?access_token=#{FFS.configuration.bitly_api_key}&longUrl=#{long_link}")
       res = JSON.parse(Net::HTTP.get(uri))
 
       res['data']['url']
